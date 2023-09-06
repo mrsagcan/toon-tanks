219de014f3b8cc0d457ddc217e7fb3845c1da878
mrsagcan <mrsagcan@yahoo.com>
2023-09-06 17:44:34 +0300

    Firing Towers

--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -27,4 +27,10 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 		float FireRange = 500.f;
 
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+
+	void CheckFireCondition();
+
+	bool InFireRange();
 };
